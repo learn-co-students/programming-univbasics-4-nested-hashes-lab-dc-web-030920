d8e29b60d9b5ef6e61951724a40cad7d11ab68cb
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-04 01:07:42 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    dennis_languages = programmer_hash[:dennis_ritchie][:languages]
+    return dennis_languages.join
+    
 end
 
 def adding_matz
@@ -79,8 +83,14 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  
     
+     newly_added = {:yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }}.merge(programmer_hash)
+    
+return newly_added
 end
 
 def changing_alan
@@ -102,6 +112,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
+     
      
 end
 
@@ -121,5 +134,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+     return programmer_hash
 
 end
